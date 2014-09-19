f4f295494a72ef68e8e9ef73c54d07a142a7e2f1
Dimitri Koussa <dimitri.koussa@gmail.com>
2014-09-19 13:12:35 +1000

    Fixed bug where nexus path was not being set correctly

--- start_as_nexus.sh
+++ start_as_nexus.sh
@@ -4,4 +4,4 @@ chown nexus:nexus /opt/sonatype-work
 if [ -z "$(ls -A /opt/sonatype-work)" ]; then
   cp -R --preserve=all /opt/sonatype-work-template /opt/sonatype-work
 fi
-su -c '/opt/start.sh "$@"' - nexus
+su -m -c '/opt/start.sh "$@"' - nexus
